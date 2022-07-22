596dbc7571b0ff7863ee0925b5ff5f9cfc37eb92
unknown <byiringoroscar@gmail.com>
2022-07-22 12:03:58 +0200

    fix send

--- home_message/views.py
+++ home_message/views.py
@@ -13,13 +13,6 @@ import threading
 
 
 # Create your views here.
-# class WhatsappThread(threading.Thread):
-#     def __init__(self, whatsapp):
-#         self.whatsapp = whatsapp
-#         threading.Thread.__init__(self)
-#
-#     def run(self):
-#         self.whatsapp.send(fail_silently=False)
 
 
 
@@ -71,64 +64,60 @@ class MessageListAPIView(ListCreateAPIView):
         data['success'] = "successfully sent"
         data['title'] = messGo.title
         data['message'] = messGo.message
-        # message_title = messGo.title
-        # message_body = messGo.message
-        # message = f'{message_title} \n----------------- \n {message_body}'
-        # send_whatsapp_message(message)
         return Response(data=data, status=status.HTTP_201_CREATED)
 
     def get_queryset(self):
         return self.queryset.filter()
 
 
-@api_view(['GET', ])
-def getMessage_view(request, slug):
-    try:
-        message_post = MessageSend.objects.get(title=slug)
-    except MessageSend.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    if request.method == "GET":
-        serializer = SendMessageSerializer(message_post)
-        return Response(serializer.data)
-
-
-@api_view(['PUT', ])
-def updateMessage_view(request, slug):
-    try:
-        message_post = MessageSend.objects.get(title=slug)
-    except MessageSend.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    if request.method == "PUT":
-        serializer = SendMessageSerializer(message_post, data=request.data)
-        data = {}
-        if serializer.is_valid():
-            serializer.save()
-            data["success"] = "update successfully"
-            return Response(data=data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['DELETE', ])
-def deleteMessage_view(request, slug):
-    try:
-        message_post = MessageSend.objects.get(title=slug)
-    except MessageSend.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    if request.method == "DELETE":
-        operation = message_post.delete()
-        data = {}
-        if operation:
-            data["success"]: "delete successfully"
-        else:
-            data['failure'] = "delete failed"
-        return Response(data=data)
-
-
-@api_view(['POST', ])
-def createMessage_view(request):
-    if request.method == "POST":
-        serializer = SendMessageSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['GET', ])
+# def getMessage_view(request, slug):
+#     try:
+#         message_post = MessageSend.objects.get(title=slug)
+#     except MessageSend.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+#     if request.method == "GET":
+#         serializer = SendMessageSerializer(message_post)
+#         return Response(serializer.data)
+#
+#
+# @api_view(['PUT', ])
+# def updateMessage_view(request, slug):
+#     try:
+#         message_post = MessageSend.objects.get(title=slug)
+#     except MessageSend.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+#     if request.method == "PUT":
+#         serializer = SendMessageSerializer(message_post, data=request.data)
+#         data = {}
+#         if serializer.is_valid():
+#             serializer.save()
+#             data["success"] = "update successfully"
+#             return Response(data=data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# @api_view(['DELETE', ])
+# def deleteMessage_view(request, slug):
+#     try:
+#         message_post = MessageSend.objects.get(title=slug)
+#     except MessageSend.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+#     if request.method == "DELETE":
+#         operation = message_post.delete()
+#         data = {}
+#         if operation:
+#             data["success"]: "delete successfully"
+#         else:
+#             data['failure'] = "delete failed"
+#         return Response(data=data)
+#
+#
+# @api_view(['POST', ])
+# def createMessage_view(request):
+#     if request.method == "POST":
+#         serializer = SendMessageSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
